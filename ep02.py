7364278134cbe01f8a378157021cf7fbb62344e1
Vitor Satyro Vitturi <vitorsv1@al.insper.edu.br>
2017-03-28 17:46:40 -0300

    pergunta_pokemon_inicial

--- ep02.py
+++ ep02.py
@@ -2,6 +2,10 @@
 import random
 import json
 
+def inspermon_inicial(ipmon_inicial):
+    meu = ipmon_inicial
+    return meu
+
 def mostra_inspermon(ipmon):
     print('Inspermon : {0}'. format(ipmon['nome']))
     print('Poder = {0}'. format(ipmon['poder']))
@@ -13,11 +17,13 @@ with open('inspermons.json') as arquivo:
     inspermons = json.load(arquivo)
 for ipmon in inspermons:
     mostra_inspermon(ipmon)
-'''
+
 while True:
     açao_usuario = input('Você pode andar e procurar Inspermons ou dormir para recuperar sua vida. \n Digite 1 para andar ou 0 para dormir: ')
     if açao_usuario == 0:
         print('Dormindo')
     elif açao_usuario == 1:
         print('Andando')
-'''
+    print('Qual pokemon deseja usar: ')
+    print(inspermon_inicial(ipmon))
+    ipmon_inicial_var = input('Digite o numero do pokemon (0 - 2): ')
