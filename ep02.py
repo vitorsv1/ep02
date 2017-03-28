32771d0c0b33e0ee83eb5ac4b20296c504360075
Vitor Satyro Vitturi <vitorsv1@al.insper.edu.br>
2017-03-28 16:42:41 -0300

    Condicional de andando e dormindo

--- ep02.py
+++ ep02.py
@@ -5,4 +5,6 @@
 while True:
     açao_usuario = input('Você pode andar e procurar Inspermons ou dormir para recuperar sua vida. \n Digite 1 para andar ou 0 para dormir: ')
     if açao_usuario == 0:
-        
+        print('Dormindo')
+    elif açao_usuario == 1:
+        print('Andando')
