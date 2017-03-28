bbebd2fb4fdaa661d5cb2548c302de411c15337d
Vitor Satyro Vitturi <vitorsv1@al.insper.edu.br>
2017-03-28 16:20:59 -0300

    Pergunta ao usuário o que ele deseja fazer

--- ep02.py
+++ ep02.py
@@ -3,4 +3,6 @@
 
 
 while True:
-    
+    açao_usuario = input('Você pode andar e procurar Inspermons ou dormir para recuperar sua vida. \n Digite 1 para andar ou 0 para dormir: ')
+    if açao_usuario == 0:
+        
